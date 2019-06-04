46c0515b375916862268c69dc0b17a44f4d8fc76
Benny Balcoon <bennybalcoon@MacBook-Air-de-Admin.local>
2019-06-04 18:33:11 +0200

    add project js

---
+++ anim.js
@@ -0,0 +1,71 @@
+// // ANIMATION EPAULE 
+
+let e = document.getElementById('epaule');
+
+e.onmouseover = function () {
+    document.getElementById('def_epaule').style.display = 'block';
+}
+
+e.onmouseout = function () {
+    document.getElementById('def_epaule').style.display = 'none';
+}
+
+// ANIMATION POIGNET
+
+let e1 = document.getElementById('poignet');
+
+e1.onmouseover = function () {
+    document.getElementById('def_poignet').style.display = "block";
+}
+
+e1.onmouseout = function () {
+    document.getElementById('def_poignet').style.display = 'none';
+}
+
+//  ANIMATION CHEVILLE
+
+let e3 = document.getElementById('cheville');
+
+e3.onmouseover = function () {
+    document.getElementById('def_cheville').style.display = 'block';
+}
+
+e3.onmouseout = function () {
+    document.getElementById('def_cheville').style.display = 'none';
+}
+
+// // ANIMATION GENOU
+
+let e4 = document.getElementById('genou');
+
+e4.onmouseover = function () {
+    document.getElementById('def_genou').style.display = 'block';
+}
+
+e4.onmouseout = function () {
+    document.getElementById('def_genou').style.display = 'none';
+}
+
+// ANIMATION HANCHE
+
+let e5 = document.getElementById('hanche');
+
+e5.onmouseover = function () {
+    document.getElementById('def_hanche').style.display = 'block';
+}
+
+e5.onmouseout = function () {
+    document.getElementById('def_hanche').style.display = 'none';
+}
+
+// ANIMATION COUDE
+
+let e6 = document.getElementById('coude');
+
+e6.onmouseover = function () {
+    document.getElementById('def_coude').style.display = 'block';
+}
+
+e6.onmouseout = function () {
+    document.getElementById('def_coude').style.display = 'none';
+}
